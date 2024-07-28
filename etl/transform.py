15eefd3cf764dd262d7354c530eb42b81f30db39
Ajuani Hall <44127995+JayHall2002@users.noreply.github.com>
2024-07-27 20:52:15 -0400

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -1,22 +1,16 @@
 # etl/transform.py
 import pandas as pd
 import os
-import zipfile
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 def transform_data():
     os.makedirs('data/processed', exist_ok=True)
     
-    # Extract the zip file
-    with zipfile.ZipFile('data/extracted/bank_account_fraud.zip', 'r') as zip_ref:
-        zip_ref.extractall('data/extracted/')
-    
     # Load the CSV file
-    df = pd.read_csv('data/extracted/bank_account_fraud.csv')
+    df = pd.read_csv('data/extracted/fraudTest.csv')
     
     # Data cleaning steps
-    # Handling missing values, duplicates, and other data quality issues
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
     
@@ -27,9 +21,6 @@ def transform_data():
     plt.savefig('data/outputs/fraud_label_distribution.png')
     plt.show()
 
-    # Additional transformations (if needed)
-    # For example: encoding categorical variables, normalization, etc.
-    
     # Save the cleaned data
     df.to_csv('data/processed/bank_account_fraud_clean.csv', index=False)
     print("Data transformation complete. Clean data saved to 'data/processed/bank_account_fraud_clean.csv'")
