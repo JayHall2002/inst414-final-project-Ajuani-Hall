f2392fe32222a11c2c9ab9de3617b768259faed8
Ajuani Hall <44127995+JayHall2002@users.noreply.github.com>
2024-07-27 20:51:43 -0400

    Update extract.py
    update to be able to use gaggle

--- etl/extract.py
+++ etl/extract.py
@@ -1,19 +1,19 @@
 # etl/extract.py
-import requests
 import os
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 def extract_data():
-    url = "https://www.kaggle.com/datasets/sgpjesus/bank-account-fraud-dataset-neurips-2022/download"
-    os.makedirs('data/extracted', exist_ok=True)
+    api = KaggleApi()
+    api.authenticate()
     
-    # Adjust the following lines based on actual download steps if necessary
-    response = requests.get(url, stream=True)
-    with open('data/extracted/bank_account_fraud.zip', 'wb') as file:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                file.write(chunk)
+    dataset = 'sgpjesus/bank-account-fraud-dataset-neurips-2022'
+    path = 'data/extracted'
+    os.makedirs(path, exist_ok=True)
     
-    print("Data extraction complete. Data saved to 'data/extracted/bank_account_fraud.zip'")
+    api.dataset_download_files(dataset, path=path, unzip=True)
+    
+    print("Data extraction complete. Files saved to 'data/extracted/'")
 
 if __name__ == "__main__":
     extract_data()
